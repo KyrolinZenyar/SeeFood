1a17b4ddd7c85896743ba60646b133049d8a6a6b
Daniel Schmidt <schmidt.152@wright.edu>
2018-11-19 13:52:45 -0500

    Attempting server push of image.

--- Project/CameraPage.cs
+++ Project/CameraPage.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using Xamarin.Forms;
 
 namespace Project
@@ -34,6 +37,8 @@ namespace Project
             NumberOfTapsRequired = 1
         };
 
+        //public static Dictionary<Image, byte[]> imagesToUpload = new Dictionary<Image, byte[]>();
+        public static Dictionary<Image, string> serverResponses = new Dictionary<Image, string>();
 
 
         public CameraPage()
@@ -60,12 +65,24 @@ namespace Project
 
         public static void TakePhoto(Stream stream)
         {
+            Dictionary<Image, byte[]> imagesToUpload = new Dictionary<Image, byte[]>();
+            MemoryStream memStream = new MemoryStream();
+            stream.CopyTo(memStream);
+            byte[] imageData = memStream.ToArray();
+            //IF THIS DOESN'T WORK, TRY SWAPPING TO THE IMAGESTREAM VERSION
+            //Stream imageStream = new MemoryStream(imageData);
             Image imageToSend = new Image
             {
-                Source = Xamarin.Forms.ImageSource.FromStream(() => stream)
+                //Source = Xamarin.Forms.ImageSource.FromStream(() => stream)
+                Source = Xamarin.Forms.ImageSource.FromStream(() => memStream)
+                //Source = Xamarin.Forms.ImageSource.FromStream(() => imageStream)
             };
+            imagesToUpload.Add(imageToSend, imageData);
             imageCount++;
             classifyButton.Text = "Classify " + imageCount + " Photos";
+            //Classify image as each photo is taken
+            ClassifyImage(imagesToUpload);
+            
         }
 
         private void GoToGallery(object sender, EventArgs e) {
@@ -80,8 +97,48 @@ namespace Project
             App.SwitchTo(page);
         }
 
+        //Send Images To AWS
+        public static async void ClassifyImage(Dictionary<Image, byte[]> imagesToUpload)
+        {
+            
+            //Defines web address to upload photos to
+            var AWSServer = "http://seefood-dev2.us-east-2.elasticbeanstalk.com/upload";
+            try
+            {
+                //Iterate through all images passed in (should only be one at a time)
+                foreach (KeyValuePair<Image, byte[]> uploadData in imagesToUpload)
+                {
+                    //Get byte array of image
+                    var byteArrayToUpload = uploadData.Value;
+
+                    //Set up HTTP client and data to upload (byte array) THIS MAY NOT WORK
+                    HttpClient serverClient = new HttpClient();
+                    MultipartFormDataContent uploadDataContent = new MultipartFormDataContent();
+                    ByteArrayContent byteArrayContent = new ByteArrayContent(byteArrayToUpload);
+                    uploadDataContent.Add(byteArrayContent);
+
+                    //Get server response
+                    var response = await serverClient.PostAsync(AWSServer, uploadDataContent);
+
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+
+                    //Put string of server response into dictionary associated with image.
+                    serverResponses.Add(uploadData.Key, responseString);
+
+                    Debug.WriteLine(responseString);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception occurred: " + ex.ToString());
+
+                return;
+            }
+
+        }
+
         private void ClassifyThese(object sender, EventArgs e) {
-            //SendStuffToAWS
+            
             imageCount = 0;
             ClassificationPage page = new ClassificationPage();
             //Other setup stuff
